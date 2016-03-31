f554879caee2ff470cf0005ceadc3e5ff6fd410a
jomaraBinda <jmb89@ist.psu.edu>
2016-03-31 12:51:02 -0400

    open chat window

--- js/usermap.js
+++ js/usermap.js
@@ -11,13 +11,18 @@
 	var latitude;
 	var longitude;
 	var currentUsername;
-	var softSlider;
+	var softSlider= document.getElementById('soft');;
 	var maxDistance;
 	var newRange = false;	
 	var cookiename; 
 	var userGeoPoint;
 	var iniValue;
-	
+    //this variable represents the total number of popups can be displayed according to the viewport width
+    var total_popups = 0;
+           
+    //arrays of popups ids
+    var popups = [];
+    
 	//verify interest list
 	var intList = [];
 	var interest = Parse.Object.extend("Interest");
@@ -148,9 +153,9 @@
 		  '<ul><li><b>Last Active:</b>' + dateString + "</li><li>" + stat + 
 		  '</li><li>' + occupation + 
 		  '</li></ul></div>'+
-		  '<button>Let`s chat!</button>'+
+		  '<button onclick="register_popup(\''+ username +'\')">Open chat</button>'+
 		  '</div>';
-//onclick="openWindow()" button event
+
 		 var infowindow = new google.maps.InfoWindow({
 		   content: contentString
 		 });
@@ -251,17 +256,6 @@
 	  return strTime;
 	}
 
-/*	function openWindow() {
-				width = 900;
-				height = 600;
-				options = 'resizable=yes';
-				name = 'openWindow';
-				window.open(
-					"https://oauth.groupme.com/oauth/authorize?client_id=RDRxcMqqrOU3Z3IO7t0FiC1GUSfJvu2gkdE8gQoId7jtIPDk",
-					false,
-					'screenX='+(screen.width-width)/2+',screenY='+(screen.height-height)/2+',width='+width+',height='+height+','+options
-				)
-	}*/
 
 	var range_all_sliders = {
 		'min': [ 50 ],
@@ -271,7 +265,7 @@
 		'max': [ 500 ]
 	};
 
-  softSlider = document.getElementById('soft');
+   
   noUiSlider.create(softSlider, {
 		start: 50,
 		range: range_all_sliders,
@@ -350,7 +344,113 @@
 		}	
 	
 	}
-	
+
+////chat/////
+//this function can remove a array element.
+Array.remove = function(array, from, to) {
+                var rest = array.slice((to || from) + 1 || array.length);
+                array.length = from < 0 ? array.length + from : from;
+                return array.push.apply(array, rest);
+            };
+
+//this is used to close a popup
+function close_popup(id)
+{
+                for(var iii = 0; iii < popups.length; iii++)
+                {
+                    if(id == popups[iii])
+                    {
+                        Array.remove(popups, iii);
+                       
+                        document.getElementById(id).style.display = "none";
+                       
+                        calculate_popups();
+                       
+                        return;
+                    }
+                }  
+            }
+       
+//displays the popups. Displays based on the maximum number of popups that can be displayed on the current viewport width
+function display_popups()
+{
+                var right = 220;
+               
+                var iii = 0;
+                for(iii; iii < total_popups; iii++)
+                {
+                    if(popups[iii] != undefined)
+                    {
+                        var element = document.getElementById(popups[iii]);
+                        element.style.right = right + "px";
+                        right = right + 320;
+                        element.style.display = "block";
+                    }
+                }
+               
+               var jjj = iii;
+                for(var jjj; jjj < popups.length; jjj++)
+                {
+                    var element = document.getElementById(popups[jjj]);
+                    element.style.display = "none";
+                }
+            }
+          
+//creates markup for a new popup. Adds the id to popups array.
+function register_popup(name)
+{
+               
+    for(var iii = 0; iii < popups.length; iii++)
+    {  
+        //already registered. Bring it to front.
+         if(id == popups[iii])
+             {
+              Array.remove(popups, iii);
+                   
+             popups.unshift(id);
+                       
+             calculate_popups();
+   
+               return;
+                }
+        }              
+               
+    var element = '<div class="popup-box chat-popup" id="'+ name +'">';
+    element = element + '<div class="popup-head">';
+    element = element + '<div class="popup-head-left">'+ name +'</div>';
+    element = element + '<div class="popup-head-right"><a href="javascript:close_popup(\''+ name +'\');">&#10005;</a></div>';
+    element = element + '<div style="clear: both"></div></div><div class="popup-messages"></div></div>';
+               
+    document.getElementsByTagName("body")[0].innerHTML = document.getElementsByTagName("body")[0].innerHTML + element; 
+       
+    popups.unshift(name);
+                       
+    calculate_popups();
+               
+}
+           
+//calculate the total number of popups suitable and then populate the toatal_popups variable.
+function calculate_popups()
+{
+                var width = window.innerWidth;
+                if(width < 540)
+                {
+                    total_popups = 0;
+                }
+                else
+                {
+                    width = width - 200;
+                    //320 is width of a single popup box
+                    total_popups = parseInt(width/320);
+                }
+               
+                display_popups();
+               
+            }
+           
+//recalculate when window is loaded and also when window is resized.
+window.addEventListener("resize", calculate_popups);
+window.addEventListener("load", calculate_popups);	
 	
 	
 	
